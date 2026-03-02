5b299eabd1f4d5616fe6895ebf34605ee24e1bfe
TranMinhQuan <jerrygaming857@gmail.com>
2026-03-02 08:48:33 +0700

    Bai3

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {}
+      <View style={styles.square}>
+
+        {}
+        <Text>Hello, world</Text>
+        
+      </View>
+
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
   },
+  square: {
+    width: 150, 
+    height: 150, 
+    backgroundColor: 'skyblue', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  }
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,11 @@
+# Bài tập Custom component - Buổi 3
+
+**Thông tin sinh viên:**
+* **Họ và tên:** Trần Minh Quân
+* **Mã sinh viên:** 23810310334
+* **Lớp:** D18CNPM5
+
+## Kết quả 
+Đây là hình ảnh kết quả:
+
+![Giao diện đăng nhập](./assets/helloWorld.png)
